aa17dd08f412549ffc80fab20675cbae10c839ab
Stu Peters <stu_peters187@hotmail.com>
2016-09-03 22:23:45 -0400

    change functional components to class components

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyAtDeS_VuXWdlzgQaO-LEFeuL_NCdTskfo'
-// create a new component. This compnent should produce some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
+
+YTSearch({key: API_KEY, term: 'snack cakes'}, function(data) {
+  console.log(data)
+});
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'yeezus'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
     );
+  }
 }
 
-// Tak this component's generated HTML and put it on the page (in the DOM)
 
 ReactDOM.render(<App />, document.querySelector('.container'));
